afda374a19c1e7019193c055e09a90b7c53e6aa3
Sany Binthe Akter <sakter@usf.edu>
2026-04-08 13:25:39 -0400

    updated

---
+++ src/components/TaskList.js
@@ -0,0 +1,17 @@
+import TaskCard from "./TaskCard";
+
+export default function TaskList({ tasks }) {
+  if (tasks.length === 0) {
+    return <p className="text-gray-400 p-4">No tasks yet!</p>;
+  }
+
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <TaskCard title={task.title} done={task.done} />
+        </li>
+      ))}
+    </ul>
+  );
+}
